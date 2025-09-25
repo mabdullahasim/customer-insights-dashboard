bbd6174a39b6a7987421f00be8008c59b1893268
mabdullahasim <kingabdullah788@gmail.com>
2025-09-24 20:39:22 -0400

    created get_current_user and get_current_active_user in core/security.py

--- src/backend/customer-insights-backend/app/core/security.py
+++ src/backend/customer-insights-backend/app/core/security.py
@@ -79,5 +79,28 @@ def create_access_token(data: dict, expires_delta: timedelta or None = None):
 
     return encoded_jwt # REturn the JWT string
 
+async def get_current_user(token str = Depends(oauth_2_scheme)): #parsing out token data
+    credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials" headers={"WWW-Authenticate": "Bearer"})
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username : str = payload.get("sub")
 
+        if username is None:
+            raise credential_exception
 
+        token_data = TokenData(username =username)
+
+    except JWTError:
+        raise credential_exception
+
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credential_exception
+
+    return user
+
+async def get_current_active_user(current_userL UserInDB = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+
+    return current_user
