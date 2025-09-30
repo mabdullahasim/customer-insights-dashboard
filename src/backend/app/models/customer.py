acd4bae0d949662fd314369fe997a0eac42c224a
mabdullahasim <kingabdullah788@gmail.com>
2025-09-30 16:57:09 -0400

    made customer model in models/customer.py

--- src/backend/app/models/customer.py
+++ src/backend/app/models/customer.py
@@ -1,17 +1,16 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from app.core.database import Base
 from datetime import datetime
-class customer(Base):
-    __tablename__ = "Customers"
 
-    customer_id = Column(Integer, primary_key=True, index=True)
+class Customer(Base):
+    __tablename__ = "customers"
+
+    User.id = Column(Integer, primary_key=True, index=True)
     full_name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
-    review = Column(Integer, nullable=False)
-    last_login = Column(DateTime, default=datetime.utcnow)
+    review = Column(Integer)
     country = Column(String, nullable=False)
     total_spent = Column(Float, nullable=False)
     purchase_frequency = Column(Float, nullable=False)
     last_purchase_date = Column(DateTime, default=datetime.utcnow)
-    total_spent = Column(Float, nullable=False)
     
--- src/backend/app/schemas/customer.py
+++ src/backend/app/schemas/customer.py
@@ -1,1 +1,7 @@
+from pydantic import BaseModel, field_validator
+def customer_create(BaseModel):
 
+
+
+
+def customer_read(BaseModel):
