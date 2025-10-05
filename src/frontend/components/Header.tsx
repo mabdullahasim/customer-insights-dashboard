924a77e7ed1bc1200904c7b441ff5c32ec13ecdc
mabdullahasim <kingabdullah788@gmail.com>
2025-10-05 04:40:36 -0400

    changed background colour in dashboard page, made it so search bar exapnds between notificiation icon when search icon clicked

--- src/frontend/components/Header.tsx
+++ src/frontend/components/Header.tsx
@@ -1,5 +1,7 @@
 import styles from "./Header.module.css";
+import notifications from "../assets/notifications.png";
 import {useState} from "react";
+import { Search } from "@mynaui/icons-react";
 
 type HeaderProps = { //blueprint that defines the prop
     onSearch?: (query: string) => void; //onSearch is a functional prop, if passed, it takes a string (query) and return nothing
@@ -7,31 +9,36 @@ type HeaderProps = { //blueprint that defines the prop
 
 const Header = ({onSearch}: HeaderProps) => { //recieves props of type HeaderProps and extracts onSearch
     const [query, setQuery] = useState(""); //Local state for the search input value with in header page does not update to main component
+    const [expanded, setExpanded] = useState(false);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => { //runs everytime user types in input
         setQuery(e.target.value) //update local query state with the input value
         if (onSearch) onSearch(e.target.value); //if parent (home) passed onSearch call it with the current input
     };
-
     return(
         <header className ={styles.header}>
             <p>Hi, Abdullah</p>
-            <div className={styles.searchBarWrapper}>
-                <input className= {styles.searchBar}
-                    type ="text"
-                    value ={query}
-                    onChange={handleChange} //calls handleChang with every keystroke
+           <div className={`${styles.iconGroup} ${expanded ? styles.expanded : ""}`}>
+                <Search
+                    className={styles.searchIcon}
+                    size={30}
+                    color="#BCFFDB"
+                    strokeWidth={1}
+                    onClick={() => setExpanded(v => !v)}
+                />
+
+                <input
+                    className={styles.searchInput}
+                    type="text"
+                    value={query}
+                    onChange={handleChange}
                     placeholder="Search..."
                 />
+
+                <button className={styles.iconButton}>
+                <img className={styles.notifications} src={notifications} alt="Notifications" />
+                </button>
             </div>
-            <button className={styles.iconButton} >
-                <img  alt="User menu" />
-            </button>
-            
-
-
-
-
         </header>
     );
 };
