95ae66ee04c25a8abe7034b119caa168c778302d
mabdullahasim <kingabdullah788@gmail.com>
2025-10-11 01:37:03 -0400

    made small changes in Nav.module.css

--- src/frontend/components/Nav.tsx
+++ src/frontend/components/Nav.tsx
@@ -1,7 +1,7 @@
 import styles from "./Nav.module.css";
+import React from "react";
 import "boxicons/css/boxicons.min.css";
-
-
+import { NavLink } from "react-router-dom";
 
 
 const Nav = () => {
@@ -20,6 +20,33 @@ const Nav = () => {
                     </div>
                     <i className={`bx bx-chevron-right ${styles.toggle}`}></i>
                 </header>
+                <div className={styles.menuBar}>
+                    <div className="menu">
+                         <aside className={styles.links}>
+                            <NavLink to="/dashboard">
+                                <i className ={`bx  bx-home ${styles.home}`}  ></i>
+                                <span>Dashboard</span>
+                            </NavLink>
+                            <NavLink to="/analytics">
+                                 <i className ={`bx bx-bar-chart-alt-2    ${styles.analytics}`}  ></i>
+      
+                                 <span>Analytics</span>
+                            </NavLink>
+                            <NavLink to="/reports">
+                                <i className ={`bx  bxs-folder-down-arrow ${styles.reports}`} > </i>
+                                <span>Reports</span>
+                            </NavLink>
+                            <NavLink to="/uploads">
+                                <i className ={`bx  bxs-file-plus ${styles.upload}`}> </i>
+                                <span>Upload</span>
+                            </NavLink>
+                            <NavLink to="/settings">
+                                <i className ={`bx  bx-cog ${styles.settings}`}> </i>
+                                <span>Settings</span>
+                            </NavLink>
+                        </aside>
+                    </div>
+                </div>
             </nav>
         
         
--- src/frontend/pages/Dashboard.tsx
+++ src/frontend/pages/Dashboard.tsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import { Link } from "react-router-dom"; // for navigation
 import styles from "./Dashboard.module.css";
 import Header from "../components/Header";
-import Nav from "../components/Nav";
+import Nav from "../components/nav";
+
 const Dashboard = () => {
     return (
         <>
             <div className={styles.container}>
                 <Header/>
                 <Nav/>
-                
                 
             
             
