058a9f02d8eed4fed0c426c5071c3de2800600cf
mabdullahasim <kingabdullah788@gmail.com>
2025-11-03 23:58:15 -0500

    Added checkTokenExpiry and isLoggedIn function in api/Auth/session.ts on the front end

--- src/frontend/api/Auth/session.ts
+++ src/frontend/api/Auth/session.ts
@@ -1,11 +1,31 @@
-import React from 'react';
 
 export function getToken(): string | null{
     return localStorage.getItem("token");
 }
 
+export function checkTokenExpiry(){
+    const token = getToken();
+    if (token !== null){
+        const payload = token.split('.')[1];
+        if (payload.length !== 3){
+            throw new Error('Invalid jwt format');
+        }
+        const base64 = payload.replace(/-/g, '+').replace(/_/g, '/');
+        const {exp} = JSON.parse(atob(base64));
+
+        return Date.now() >= exp *1000;
+    }
+};
 
 export const isLoggedIn =() =>{
-    
+    const token = getToken();
+    if (token === null) {
+        return false;
+    }
+    if (!checkTokenExpiry()){
+        localStorage.removeItem("token");
+        return false;
+    }
+    return true;
 };
 
