a10efb347f04d0dd948e07160fe0adccce7ccf7d
mabdullahasim <kingabdullah788@gmail.com>
2026-04-23 00:36:41 -0400

    created home pages for analytics, data upload, reports and settings

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -3,6 +3,11 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import RequireAuth from "./api/Auth/RequireAuth";
+import Analytics from "./pages/Analytics";
+import Reports from "./pages/Reports";
+import DataUpload from "./pages/DataUpload";
+import Settings from "./pages/Settings";
+
 function App() {
   return (
     <Router>
@@ -10,7 +15,10 @@ function App() {
         {/* <Route element = {<RequireAuth/>}> for protected routes requiring auth */}
           <Route path= "/dashboard" element = {<Dashboard/>} />
         {/* </Route> */}
-
+        <Route path="/reports" element = {<Reports/>} />
+        <Route path="/settings" element ={<Settings/>} />
+        <Route path="/uploads" element={<DataUpload/>}/>
+        <Route path="/analytics" element = {<Analytics/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={<Login />} />
