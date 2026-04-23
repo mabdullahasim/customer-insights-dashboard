f60ea04ec9fa4a65ba86d55ba721d43e2a54dc50
mabdullahasim <kingabdullah788@gmail.com>
2026-04-23 00:07:57 -0400

    created api routes for customers crud functions

--- src/backend/app/api/customers.py
+++ src/backend/app/api/customers.py
@@ -1,1 +1,43 @@
- 
+from app.models.customer import Customer
+from app.schemas.customer import *
+
+from fastapi import APIRouter, status, Depends, HTTPException
+from app.core.security import get_user_by_email, get_password_hash
+from sqlalchemy.orm import Session
+from app.crud.customer import *
+from fastapi.security import OAuth2PasswordRequestForm
+from app.core.database import get_db
+from typing import List
+from app.core.security import get_current_active_user
+
+
+router = APIRouter(prefix="/customers", tags=["customers"])
+
+@router.get("/", response_model=list[CustomerRead])
+async def list(skip: int = 0, current_user=Depends(get_current_active_user), db: Session=Depends(get_db)):
+    return list_customers(db, skip, current_user.id)
+
+@router.get("/{customer_id}", response_model=CustomerRead)
+async def get_one_customer(customer_id: int, current_user=Depends(get_current_active_user), db: Session=Depends(get_db)):
+    customer = get_customer(db, customer_id, current_user.id)
+    if not customer:
+        raise HTTPException(status_code=404, detail="Customer not found")
+
+    return customer
+
+@router.put("/{customer_id}", response_model=CustomerRead)
+async def update(customer_id: int, updates: CustomerUpdate, current_user=Depends(get_current_active_user), db: Session=Depends(get_db)):
+    customer = customer_update(db, customer_id, current_user.id, updates)
+    if not customer:
+        raise HTTPException(status_code=404, detail="Customer not found")
+
+    return customer
+
+
+@router.delete("/{customer_id}", response_model=CustomerRead)
+async def delete(customer_id: int, current_user=Depends(get_current_active_user), db: Session=Depends(get_db)):
+    customer = customer_delete(db, customer_id, current_user.id)
+    if not customer:
+        raise HTTPException(status_code=404, detail="Customer not found")
+
+    return None
