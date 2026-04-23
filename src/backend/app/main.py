401a04dd1cf35351b1c8808739c92e91a467e2c6
mabdullahasim <kingabdullah788@gmail.com>
2026-04-23 00:30:22 -0400

    added routers for customers and csv parser

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from app.api import auth
 from app.api import secure
+from app.api import customers
+from app.api import csv_parser
+
 from app.core.database import engine, Base
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -21,6 +24,9 @@ Base.metadata.create_all(bind=engine)
 # Include routers
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(secure.router)
+app.include_router(customers.router)
+app.include_router(csv_parser.router)
+
 
 # Test endpoint
 @app.get("/test")
