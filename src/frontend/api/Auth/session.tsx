7998086151ba205ddc4b7aebb25492654e68f009
mabdullahasim <kingabdullah788@gmail.com>
2026-07-06 23:31:40 -0400

    fixed bug not correctly parsing the jwt token

--- src/frontend/api/Auth/session.tsx
+++ src/frontend/api/Auth/session.tsx
@@ -3,29 +3,30 @@ export function getToken(): string | null{
     return localStorage.getItem("token");
 }
 
-export function checkTokenExpiry(){
+export function checkTokenExpiry(): boolean {
     const token = getToken();
-    if (token !== null){
-        const payload = token.split('.')[1];
-        if (payload.length !== 3){
-            throw new Error('Invalid jwt format');
-        }
-        const base64 = payload.replace(/-/g, '+').replace(/_/g, '/');
-        const {exp} = JSON.parse(atob(base64));
+    if (token === null) return true;
 
-        return Date.now() >= exp *1000;
+    const parts = token.split('.');
+    if (parts.length !== 3) {
+        throw new Error('Invalid jwt format');
     }
+
+    const base64 = parts[1].replace(/-/g, '+').replace(/_/g, '/');
+    const { exp } = JSON.parse(atob(base64));
+
+    return Date.now() >= exp * 1000;
 }
 
-export const isLoggedIn =() =>{
+export const isLoggedIn = () => {
     const token = getToken();
-    if (token === null) {
-        return false;
-    }
-    if (!checkTokenExpiry()){
+    if (token === null) return false;
+
+    if (checkTokenExpiry()) {
         localStorage.removeItem("token");
         return false;
     }
+
     return true;
 }
 
